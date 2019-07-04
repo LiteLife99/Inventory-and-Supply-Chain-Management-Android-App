536ef9bffe243fac07a319743386e442465d15fd
ajdesh <ajdesh2000@gmail.com>
2019-07-04 14:29:42 +0530

    radio button added

--- PM/app/src/main/java/com/icos/podarmills/CreateUserPage.java
+++ PM/app/src/main/java/com/icos/podarmills/CreateUserPage.java
@@ -7,23 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.icos.podarmills.R;
 
+import java.util.HashMap;
+
 public class CreateUserPage extends AppCompatActivity {
-    EditText username,password;
+    EditText name,phonenumber,username,password;
     Button createUser;
+    RadioButton RadioselectedButton;
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user_page);
+        radioGroup=findViewById(R.id.radio_group_id2);
         username=findViewById(R.id.username2);
         password=findViewById(R.id.password2);
+        name=findViewById(R.id.name_id);
+        phonenumber=findViewById(R.id.mobile);
         createUser=findViewById(R.id.create_user);
         createUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +45,13 @@ public class CreateUserPage extends AppCompatActivity {
                         {
                             Toast.makeText(getApplicationContext(),"Signup Successful",Toast.LENGTH_LONG).show();
                             startActivity(new Intent(CreateUserPage.this, MainActivity.class));
+                            RadioselectedButton=findViewById(radioGroup.getCheckedRadioButtonId());
+
+                            HashMap<String,Object> hashMap=new HashMap();
+                            hashMap.put("type",RadioselectedButton.getText().toString());
+                            hashMap.put("name",name.getText().toString());
+                            hashMap.put("phonenumber",phonenumber.getText().toString());
+                            FirebaseFirestore.getInstance().collection("users").document(username.getText().toString()).set(hashMap);
                         }
                         else
                         {
