7a3f5a565266744c95481e1d1fccf943279da1ee
LiteLife <arnabmu1999@gmail.com>
2019-07-05 13:11:55 +0530

    Add new inventory page

---
+++ PM/app/src/main/java/com/icos/podarmills/AddInventoryActivity.java
@@ -0,0 +1,29 @@
+package com.icos.podarmills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddInventoryActivity extends AppCompatActivity {
+
+    private EditText edtInventoryName;
+    private EditText edtInventoryDescription;
+    private EditText edtMaintenanceDate;
+    private Button addNewInventory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_inventory);
+
+        edtInventoryName = findViewById(R.id.edtInventoryName);
+        edtInventoryDescription = findViewById(R.id.edtInventoryDescription);
+        edtMaintenanceDate = findViewById(R.id.edtMaintenanceDate);
+        addNewInventory = findViewById(R.id.addNewInventory);
+
+        //Ajinkya add these details to firebase
+
+    }
+}
