b3f1c8c559d169acd61adec2b8b0a0772b7c8a1b
LiteLife <arnabmu1999@gmail.com>
2019-07-09 13:50:38 +0530

    ViewComplaintsPage

--- PM/app/src/main/java/com/icos/podarmills/ViewComplaintsPage.java
+++ PM/app/src/main/java/com/icos/podarmills/ViewComplaintsPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -32,6 +33,13 @@ public class ViewComplaintsPage extends AppCompatActivity {
         complaintNames = new ArrayList<>();
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, complaintNames);
         complaintsList.setAdapter(adapter);
+
+        complaintsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                txtViewComplaints.setText();
+            }
+        });
 
         btnWriteComplaint.setOnClickListener(new View.OnClickListener() {
             @Override
